fc7a4f9db866983ecd26501abed7ec6f49d521ab
wecbrams <wecbrams@gmail.com>
2024-05-31 15:03:53 +0300

    Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Creates a file in /tmp
+#Create a file in /tmp
 
 file { 'school':
-  ensure  => 'present',
-  content => 'I love Puppet',
-  group   => 'www-data',
-  mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/school',
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# Installs puppet-lint
+# install flask using Puppet
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['/usr/bin', '/bin'],
+    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
+exec { 'install_werkzeug':
+    command => '/usr/bin/pip3 install werkzeug==2.1.1',
+    path    => ['/usr/bin', '/bin'],
+    unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
